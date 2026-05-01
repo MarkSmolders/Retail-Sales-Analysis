8f439ef8960c80ca674abdb40e7857f85606e946
MarkSmolders244748 <marksmolders09@gmail.com>
2026-05-01 12:17:01 +0200

    Fixed missing semicolon

--- sql/06_cleaning_customer_table.sql
+++ sql/06_cleaning_customer_table.sql
@@ -1,16 +1,19 @@
 -- Purpose: Clean the customer table by standardising country column, deduplicating and creating a customer_id primary key
--- Date: 25/04/2026
+-- Date: 01/05/2026
 
-DROP TABLE IF EXISTS dbo.customer_clean
+DROP TABLE IF EXISTS dbo.customer_clean;
 
-WITH deduped AS (
-    SELECT DISTINCT
-        first_name,
-        last_name,
-        email,
-        phone,
-        city,
-        CASE
+WITH
+    deduped
+    AS
+    (
+        SELECT DISTINCT
+            first_name,
+            last_name,
+            email,
+            phone,
+            city,
+            CASE
             WHEN LOWER(country) = 'nl'              THEN 'Netherlands'
             WHEN LOWER(country) = 'nederland'        THEN 'Netherlands'
             WHEN LOWER(country) = 'nld'              THEN 'Netherlands'
@@ -18,9 +21,9 @@ WITH deduped AS (
             WHEN LOWER(country) = 'netherlands'      THEN 'Netherlands'
             ELSE country
         END AS country,
-        date_of_birth
-    FROM dbo.customer_raw
-)
+            date_of_birth
+        FROM dbo.customer_raw
+    )
 SELECT
     ROW_NUMBER() OVER (ORDER BY first_name, last_name) AS customer_id,
     first_name,
